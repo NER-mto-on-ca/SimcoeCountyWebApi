80ed10a43b87c80c1d106bb22396a17e6efbaef2
Tom Reed <58078313+reed-tom@users.noreply.github.com>
2020-10-05 11:55:08 -0400

    added psudo code for users route

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,7 @@ router.get("/test", function(req, res, next) {
   console.log("test");
   res.send("respond with a resource");
 });
-
+//Get user - accepts ID, returns user object (id, username, email)
+//Save Map - accept json object, return true/false for success or fail
+//Get Map - accepts user id, returns json object
 module.exports = router;
